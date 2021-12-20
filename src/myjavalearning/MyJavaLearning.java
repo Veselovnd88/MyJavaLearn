bc65ace1a11b982f2e54a5ba4e4a1a1e7478571c
'Nikolay' <veselovnd@gmail.com>
2021-12-20 22:00:07 +0300

    43

--- src/myjavalearning/MyJavaLearning.java
+++ src/myjavalearning/MyJavaLearning.java
@@ -3,18 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package myjavalearning;
+import org.apache.commons.lang3.StringUtils;
 
-/**
- *
- * @author Nikolay
- */
+
+class App {
+    // BEGIN (write your solution here)
+    public static boolean isPalindrome(String str){
+        return str.equals(StringUtils.reverse(str));
+    }
+    // END
+}
 public class MyJavaLearning {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(App.isPalindrome("AMA"));
     }
     
 }
