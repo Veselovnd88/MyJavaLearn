890d86a194b46e6d21680b0d5efbd18cf88a631b
'Nikolay' <veselovnd@gmail.com>
2021-12-26 15:13:24 +0300

    javarush tasks

--- src/myjavalearning/MyJavaLearning.java
+++ src/myjavalearning/MyJavaLearning.java
@@ -1,7 +1,7 @@
 
 package myjavalearning;
 import org.apache.commons.lang3.StringUtils;
-
+import java.util.Scanner;
 
 class App {
     // BEGIN (write your solution here)
@@ -27,6 +27,50 @@ class App {
     		return null;
     	}
     }
+public static void printNumbers(int num) {
+	while (num>0) {
+		System.out.println(num);
+		num--;
+	}
+	System.out.println("finished!");
+}
+
+public static String encrypt(String str) {
+    // BEGIN (write your solution here)
+    String result = "";
+    if (str.length()%2==0){
+    for (int i = 0; i<str.length()-1;i+=2){
+        result+=str.charAt(i+1);
+        result+=str.charAt(i);
+        //System.out.println(result);
+    }}
+    else {
+          for (int i = 0; i<str.length()-2;i+=2){
+        result+=str.charAt(i+1);
+        result+=str.charAt(i);}
+        result+=str.charAt(str.length()-1);  
+    }
+    return result;
+    // END
+}
+public static void withscanner() {
+	Scanner scanner = new Scanner (System.in);
+	int sum = 0;
+	boolean isExit = false;
+	while(!isExit) {
+		if (scanner.hasNextInt()) {
+			int num = scanner.nextInt();
+			sum+=num;
+		}
+		if (scanner.hasNextLine()) {
+			if (scanner.nextLine().equals("ENTER")) {
+				isExit = true;
+			}
+		}
+	}
+	scanner.close();
+	System.out.println(sum);
+}
 }
 public class MyJavaLearning {
 
@@ -34,11 +78,14 @@ public class MyJavaLearning {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(App.normalizeUrl("HateifsReality"));//50/62
-        System.out.println(App.isPalindrome("AMA"));
-        System.out.println(App.getNumberExplanation(0));
-        System.out.println(App.getNumberExplanation(42));
-        System.out.println(App.getNumberExplanation(666));
+        //System.out.println(App.normalizeUrl("HateifsReality"));//50/62
+        //System.out.println(App.isPalindrome("AMA"));
+        //System.out.println(App.getNumberExplanation(0));
+        //System.out.println(App.getNumberExplanation(42));
+        //System.out.println(App.getNumberExplanation(666));
+    	//App.printNumbers(5);
+    	//System.out.println(App.encrypt("attack"));
+    	App.withscanner();
     }
     
 }
