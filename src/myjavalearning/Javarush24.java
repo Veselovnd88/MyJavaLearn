68e5604fc5cd7758d2ce803a274b949611607a9e
nikolay <nikolay@nikolay-VivoBook-ASUSLaptop-X421JAY-K413JA>
2022-01-05 17:02:12 +0300

    multyply

--- src/myjavalearning/Javarush24.java
+++ src/myjavalearning/Javarush24.java
@@ -101,12 +101,22 @@ public class Javarush24 {
 		System.out.print(Arrays.toString(strings));
 		scanner.close();
 	}
-	
+	public static void multiplytable(){
+		int[][] mytable = new int[10][10];
+		for(int x =0; x<10;x++) {
+			for(int y=0;y<10;y++) {
+				mytable[x][y] = (x+1)*(y+1);
+				System.out.print(mytable[x][y]+" ");
+			} System.out.println();
+		}
+		
+	}
 	public static void main(String[] args) {
 		//  2 уровень,  4 лекция
 		//reverse();
 		//maximum();
-		deleteequalsstr();
+		//deleteequalsstr();
+		//multiplytable();
 	}
 
 }
