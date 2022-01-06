e215393bf109c9db430771ceb0803e0c617cdb13
'Nikolay' <veselovnd@gmail.com>
2022-01-06 20:48:10 +0300

    car plaing

--- src/myjavalearning/Cars.java
+++ src/myjavalearning/Cars.java
@@ -4,7 +4,22 @@ import java.time.LocalDate;
 import java.time.Period;
 
 class Car{
+	public Car(){}// default constructor
+	public Car(String name){
+		this(name,0,0,false);}
+	
+	public Car(String name, int newspeed,int fuel, boolean eng ){
+		setName(name);
+		setSpeed(newspeed);
+		setGasoline(fuel);
+		setEngineState(eng);
+
+	}
 	private String name;
+	public void setPressure(int...pressures) {
+		int i;
+		i=10;
+	}
 	public String getName() {
 		return name;
 	}
@@ -21,7 +36,7 @@ class Car{
 		return speed;
 	}
 	public void setSpeed(int speed) {
-		if (engineState && (gasoline>0)) {
+		//if (engineState && (gasoline>0)) {
 			if (speed<0) {
 				System.out.println("Speed can't be negative, please try again, Speed set to Zero");
 				this.speed = 0;}
@@ -30,17 +45,17 @@ class Car{
 				this.speed = 300;}
 			else {
 				this.speed = speed;}
-	}
-		else{
-			if (!engineState) {			
-			System.out.println("Please, start engine");
-			this.speed = 0;}
-			else {
-				if (gasoline<=0) {
-				System.out.println("Need more fuel");
-				this.speed = 0;
-			}
-		}}
+	//}
+		//else{
+			//if (!engineState) {			
+			//System.out.println("Please, start engine");
+			//this.speed = 0;}
+			//else {
+			//	if (gasoline<=0) {
+			//	System.out.println("Need more fuel");
+			//	this.speed = 0;
+			//}
+		//}//}
 		}
 	public int getGasoline() {
 		return gasoline;
@@ -81,20 +96,26 @@ public class Cars {
 	public static void main(String[] args) {
 		Car car_one = new Car();
 		car_one.setOdo(143);
+		System.out.println(car_one.getOdo());
+		Car car2 = new Car("Crazy",60,100,true);
+		System.out.println(car2.getSpeed());
+		System.out.printf("%s car moves with speed %d and fuel level %d litres %n",car2.getName(),
+				car2.getSpeed(), car2.getGasoline());
 		
-		car_one.setName("Vasya");
-		car_one.setManufactured(LocalDate.of(1900,01,01));
-		//Запуск двигателя
-		car_one.setEngineState(true);
-		//Заправка
-		car_one.setGasoline(500);
-		System.out.printf("%s car was manufactured in %d%n", car_one.getName(),car_one.getManufactured().getYear());
-		int[] speedlist = new int[] {10,2000,30,100,0,-5,-100,250};
-		for(int sp:speedlist) {
-			car_one.setSpeed(sp);
-		
-		System.out.printf("%s car moves with speed %d%n", car_one.getName(), car_one.getSpeed());
-	}
+		/*
+		 * 
+		 * 
+		 * car_one.setName("Vasya"); car_one.setManufactured(LocalDate.of(1900,01,01));
+		 * //Запуск двигателя car_one.setEngineState(true); //Заправка
+		 * car_one.setGasoline(500);
+		 * System.out.printf("%s car was manufactured in %d%n",
+		 * car_one.getName(),car_one.getManufactured().getYear()); int[] speedlist = new
+		 * int[] {10,2000,30,100,0,-5,-100,250}; for(int sp:speedlist) {
+		 * car_one.setSpeed(sp);
+		 * 
+		 * System.out.printf("%s car moves with speed %d%n", car_one.getName(),
+		 * car_one.getSpeed()); }
+		 */
 	}
 
 }
